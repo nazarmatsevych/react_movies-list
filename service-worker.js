3ce3db23eebcf501d5db17c7a4b8cb18432f204c
Nazar Matsevych <nazar6192111@gmail.com>
2021-05-18 17:14:10 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.2864373d6bc61c7e48f9e43b143db318.js"
+  "./precache-manifest.73e853c1ac9860468b5507081599e782.js"
 );
 
 self.addEventListener('message', (event) => {
